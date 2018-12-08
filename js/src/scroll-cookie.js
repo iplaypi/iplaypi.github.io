41691ef4300b82da7e2b412fbeb7730e44dd2709
iplaypi <1305098770@qq.com>
2018-12-09 03:39:10 +0800

    Travis CI Auto Builder at 2018-12-09 03:39

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sat Dec 08 2018 18:26:53 GMT+0800 (China Standard Time)
+// build time:Sun Dec 09 2018 03:39:01 GMT+0800 (China Standard Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
