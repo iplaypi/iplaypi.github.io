501059d8128f82c38239c39ebc952e584f2f297a
iplaypi <playpi@qq.com>
2020-02-12 22:43:38 +0800

    Travis CI Auto Builder at 2020-02-12 22:43

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Wed Feb 12 2020 01:41:52 GMT+0800 (China Standard Time)
+// build time:Wed Feb 12 2020 22:42:58 GMT+0800 (China Standard Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
